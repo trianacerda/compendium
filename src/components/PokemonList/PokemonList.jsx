377ef881e81900c5a2bf296a0ab7f8d33acc0cf7
Triana Cerda <trianaRcerda@gmail.com>
2021-12-02 12:50:37 -0800

    added pokemonlist

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,2 +1,31 @@
 import PropTypes from 'prop-types';
-import
+import PokemonDetail from '../PokemonDetail/PokemonDetail';
+
+export default function PokemonList({ pokedex }) {
+  return (
+    <ul aria-label="pokemon-list" className="pokemon-List">
+      {pokedex.map((pokemon) => {
+        return (
+          <li className="single-pokemon" key={pokemon.id}>
+            <PokemonDetail pokemon={pokemon} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+PokemonDetail.propTypes = {
+  pokemon: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired,
+      pokedex: PropTypes.string.isRequired,
+      shape: PropTypes.string.isRequired,
+      typeOne: PropTypes.string.isRequired,
+      typeTwo: PropTypes.string.isRequired,
+      speed: PropTypes.string.isRequired,
+    }).isRequired
+  ),
+};
