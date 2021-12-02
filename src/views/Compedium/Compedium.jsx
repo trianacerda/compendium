e41cea8eef9b6406cb32c80a69bb5e3ae596e5c0
Triana Cerda <trianaRcerda@gmail.com>
2021-12-02 10:22:57 -0800

    rendering Compendium container to DOM

---
+++ src/views/Compedium/Compedium.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { useState, useEffect } from 'react';
+
+export default function Compendium() {
+  return (
+    <>
+      <h1>Pokémon Compendium</h1>
+    </>
+  );
+}
